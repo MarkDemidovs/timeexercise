4d3cd7ff22bf947a86b55b4a9571008a77a8a8e1
Mark <markdemidovs@proton.me>
2024-10-30 17:12:43 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default function App() {
+  return (<>
+    <h1>Hello World</h1>
+    <Button variant="primary">Primary</Button>{' '}
+    </>
+  );
+}

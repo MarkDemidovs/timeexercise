ce45f1e5fab257809f6dde12f90a74cd17f5b24c
Mark <markdemidovs@proton.me>
2024-10-30 17:35:50 +0200

    bootstrap support + navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 export default function App() {
   return (<>
     <h1>Hello World</h1>
-    <Button variant="primary">Primary</Button>{' '}
+    <Button variant="primary">Start</Button>
+    <Button variant="danger ">Stop</Button>
+
     </>
   );
 }
---
+++ src/NavBar.tsx
@@ -0,0 +1,5 @@
+export default function Navbar() {
+    return (<div className="navbar">
+        <h3 id="navText" className="text-light mx-auto my-auto">timeexercise</h3>
+    </div>)
+}

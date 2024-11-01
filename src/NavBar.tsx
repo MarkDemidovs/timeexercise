cdd48a7b4ea562a8259766a2131e72597051818c
Mark <markdemidovs@proton.me>
2024-11-01 21:42:51 +0200

    Update NavBar.tsx

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,5 +1,5 @@
 export default function Navbar() {
     return (<div className="navbar">
-        <h3 id="navText" className="text-light mx-auto my-auto">timeexercise</h3>
+        <h3 id="navText" className="text-light mx-auto my-auto">timeexercise by markdemidovs on twitter</h3> 
     </div>)
 }
